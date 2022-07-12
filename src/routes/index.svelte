3b56242415d8fe955abd6ecb261e1745ecdb53ab
yuxxeun <itsyuxxeun@gmail.com>
2022-07-13 04:03:36 +0700

    first

---
+++ src/routes/index.svelte
@@ -0,0 +1,2 @@
+<h1>just lil SvelteKit app with bun</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ svelte.config.js
@@ -0,0 +1,8 @@
+
+import adapter from 'svelte-adapter-bun';
+
+export default {
+  kit: {
+    adapter: adapter()
+  }
+};
