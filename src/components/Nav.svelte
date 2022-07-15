a0d23f6aec40111c9ce1fe7932879f1194bcc93a
yuxxeun <itsyuxxeun@gmail.com>
2022-07-16 00:04:23 +0700

    comps: Header & Nav

---
+++ src/components/Nav.svelte
@@ -0,0 +1,3 @@
+<section>
+	<p>Nav</p>
+</section>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../app.css';
 	import Footer from '../components/Footer.svelte';
+	import Nav from '../components/Nav.svelte';
 </script>
 
 <section class="px-10 mx-auto my-5 items-center py-5">
+	<Nav />
 	<slot />
 	<Footer />
 </section>
