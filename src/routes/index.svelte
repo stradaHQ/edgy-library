d46bcadff20ebdfaaedccd4600abce740d5a51ea
yuxxeun <itsyuxxeun@gmail.com>
2022-07-16 00:24:17 +0700

    routes: store

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,14 +8,20 @@
 	}
 </script>
 
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-{#await getData()}
-	<p>Fetching data...</p>
-{:then data}
-	{#each data as game}
-		<li>{game.title}</li>
-	{/each}
-{:catch error}
-	<p>Something went wrong while fetching the data:</p>
-	<pre>{error}</pre>
-{/await}
+<svelte:head>
+	<title>edgy library</title>
+</svelte:head>
+
+<section>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	{#await getData()}
+		<p>Fetching data...</p>
+	{:then data}
+		{#each data as game}
+			<li>{game.title}</li>
+		{/each}
+	{:catch error}
+		<p>Something went wrong while fetching the data:</p>
+		<pre>{error}</pre>
+	{/await}
+</section>
---
+++ src/routes/store.svelte
@@ -0,0 +1,1 @@
+<section>Add some books to read</section>
